2f766b80a6854f061082513ec2307408231f3bba
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-03-25 15:57:54 +0300

    init: initial commit

---
+++ src/main/java/com/skecorp/employmentmgmtsystem/EmploymentmgmtsystemApplication.java
@@ -0,0 +1,13 @@
+package com.skecorp.employmentmgmtsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentmgmtsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentmgmtsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skecorp/employmentmgmtsystem/EmploymentmgmtsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skecorp.employmentmgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentmgmtsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
